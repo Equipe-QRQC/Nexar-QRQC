942f3cd380b760928eea52af4cb7dd01e783c12d
enzo Jimenez <enzoji99@gmail.com>
2025-03-21 22:06:23 -0300

    [QRQC-Numero] Implementação da nova funcionalidade de pull no jira

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!, me chamo eNZO"
+    return "Hello, Flask!, me chamo ENZO"
 
 if __name__ == '__main__':
     app.run(debug=True)
